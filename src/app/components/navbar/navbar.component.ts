92c6cbc668ec41d2481110e5e1344653276ec257
Ignacio Carvajal Duran <ignacio.carvajal@inacapmail.cl>
2018-11-05 14:44:42 -0300

    navbar fixed update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,36 @@
-import {  Component, HostListener, Inject, OnInit, ViewChild,ElementRef } from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';  
+import { trigger, state, transition, style, animate } from '@angular/animations';  
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  animations:[ 
+    trigger('fade',
+    [ 
+      state('void', style({ opacity : 0})),
+      transition(':enter',[ animate(300)]),
+      transition(':leave',[ animate(500)]),
+    ]
+  )]
 })
 export class NavbarComponent implements OnInit {
   public isCollapsed = true;
 
-  
-
-  sticky: boolean = false;
-  constructor(  ) { }
+  constructor(@Inject(DOCUMENT) document) { }
 
   ngOnInit() { }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+     if (window.pageYOffset > 550) {
+       let element = document.getElementById('navbar');
+       element.classList.add('sticky');
+     } else {
+      let element = document.getElementById('navbar');
+        element.classList.remove('sticky'); 
+     }
+  }
 
 }
